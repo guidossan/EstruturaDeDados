214b0dde3b26daac832d4653ac5b793ba6d918ec
guilherme <guimsl2004@gmail.com>
2023-08-26 15:57:41 -0300

    Estrutura de pilha e reorganizacao de vetor

--- Aula02.java
+++ Aula02.java
@@ -1,2 +1,35 @@
 public class Aula02 {
+        public static void main(String[] args) {
+//        int [] a = new int[6];
+//        a[0] = 10;
+//        a[1] = 25;
+//        a[2] = 5;
+//        a[3] = 3;
+//        a[4] = 50;
+//        a[5] = 14;
+//        int aux =0;
+//        for(int i = 0; i<6;i++){
+//            for (int j =  i+1; j<6;j++){
+//                if(a[i]>a[j]){
+//                    aux=a[i];
+//                    a[i] = a[j];
+//                    a[j] = aux;
+//                }
+//            }
+//        }
+//        for (int z = 0; z<6; z++){
+//        System.out.println(a[z]);
+//        }
+            Pilha p = new Pilha();
+            p.empilhar(1);
+            p.empilhar(2);
+            p.empilhar(3);
+            p.empilhar(445);
+            p.empilhar(2);
+            p.empilhar(1);
+            p.imprimepilha();
+        }
+
+
+
 }
--- Pilha.java
+++ Pilha.java
@@ -1,2 +1,31 @@
-public class Pilha {
+
+    public class Pilha {
+        private int topo;
+        private int dado[];
+        public Pilha(){
+            this.topo = -1;
+            dado = new int[5];
+        }
+        public void empilhar(int valor){
+            if(topo<dado.length){
+                topo++;
+                dado[topo]=valor;
+            }else{
+                System.out.println("Pilha cheia");
+            }
+
+        }
+        public void vazia(){
+            if(topo == -1){
+                System.out.println("Pilha vazia");
+            }else {
+                System.out.println("Valor do topo: " + dado[topo]);;
+            }
+        }
+        public void imprimepilha(){
+            for (int i = topo; i > -1; i--) {
+                System.out.println(dado[i]);
+            }
+        }
+
 }
